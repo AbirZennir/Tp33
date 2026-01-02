59c676ff6b97aa0d9a8a964300a8ace7ecb02386
abir_zennir <abirzennir3@gmail.com>
2026-01-02 09:27:06 +0100

    add

--- src/main/java/com/example/demok8s/DemoK8sApplication.java
+++ src/main/java/com/example/demok8s/DemoK8sApplication.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Tp31Application {
-
+public class DemoK8sApplication {
     public static void main(String[] args) {
-        SpringApplication.run(Tp31Application.class, args);
+        SpringApplication.run(DemoK8sApplication.class, args);
     }
-
 }
